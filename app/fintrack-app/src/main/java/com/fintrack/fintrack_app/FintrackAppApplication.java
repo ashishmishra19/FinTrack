8d9789f771213945383ca75a87227e373f9ce4c6
Ashish Mishra <ashishmishra@Ashishs-Mac.local>
2025-07-15 15:55:17 +0530

    Initial Spring Boot scaffold for FinTrack App

---
+++ app/fintrack-app/src/main/java/com/fintrack/fintrack_app/FintrackAppApplication.java
@@ -0,0 +1,13 @@
+package com.fintrack.fintrack_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintrackAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintrackAppApplication.class, args);
+	}
+
+}
---
+++ app/fintrack-app/src/test/java/com/fintrack/fintrack_app/FintrackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintrack.fintrack_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrackAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
